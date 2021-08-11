b9c723b37a183f4202e3e931aa20c2977d4d6694
Code done right <sciencedoneright5@gmail.com>
2021-08-11 20:58:59 +0530

    Changed some extremely small details

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import discord
 from discord.ext import commands
 
 # BOT USER INFO #
-
 coinbot = commands.Bot(command_prefix = ('coin ', 'Coin ', 'coin.', 'Coin.'))
 INVITE_URL = r'https://discord.com/api/oauth2/authorize?client_id=815556341766553600&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.events.stdlib.com%2Fdiscord%2Fauth%2F&scope=bot'
 
 # COMMAND COLORS #
-
 NORMAL = 0x006AFF
 SUCCESSFUL = 0x29CC00
 ERROR = 0x961515
