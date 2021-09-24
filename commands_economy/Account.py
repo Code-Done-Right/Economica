30a055770c02149d84a6f78c94d1f20565ddedb6
Science Done Right <sciencedoneright5@gmail.com>
2021-09-24 10:25:49 +0530

    Changed account.py. Currently, only Experimentation is going on

--- commands_economy/Account.py
+++ commands_economy/Account.py
@@ -1,5 +1,9 @@
-async def OpenAccount():
-    pass
+async def OpenAccount(db_connection, username):
+    USER_CONN = db_connection
+    
+    user = await USER_CONN.fetchrow('SELECT * FROM users WHERE username = $1', username)
+    print(user)
+    
 
-async def GetBankData():
+async def GetBankData(db_connection):
     pass
