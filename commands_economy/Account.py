321798b27bebea7eaba07de2dc918c9822fa80b5
Science Done Right <sciencedoneright5@gmail.com>
2021-09-30 21:39:14 +0530

    Minor changes in account.py

--- commands_economy/Account.py
+++ commands_economy/Account.py
@@ -1,13 +1,13 @@
 import asyncio
 import asyncpg
 
-async def OpenAccount(db_connection, user_id, username, discriminator) -> bool:
+async def OpenAccount(db_connection, user_id) -> bool:
     """
-        OpenAccount checks if there is an existing account with the same name and discriminator.
+        OpenAccount checks if there is an existing account with the same ID.
         If there is one, creating a new account should not be allowed so therefore it returns False.
         Function Annotation:
 
-        True: When there is no user in the db with the username and discriminator given.
+        True: When there is no user in the db with the ID given.
         False: When there is an existing account.
     """
 
@@ -19,13 +19,13 @@ async def OpenAccount(db_connection, user_id, username, discriminator) -> bool:
     else:
         await USER_CONN.execute('''
             INSERT INTO economica_bank(user_id, wallet, bank) VALUES($1, $2, $3) ON CONFLICT DO NOTHING
-            ''', user_id, '0', '0')
+            ''', user_id, 0, 0)
 
     return True
 
-async def GetBankData(db_connection):
+async def GetBankData(db_connection) -> None:
     """
         GetBankData returns the info of a specific database.
-        Right now, I don't know whether to keep this.
+        Right now, I don't know whether this will be useful.
     """
     USER_CONN = db_connection
